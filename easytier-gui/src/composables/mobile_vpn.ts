c95667b8f069671906f2782e8c4ec89703224b30
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2025-12-30 02:20:50 +0800

    fix(android): remove self-exclusion from tun to support quic

--- easytier-gui/src/composables/mobile_vpn.ts
+++ easytier-gui/src/composables/mobile_vpn.ts
@@ -58,7 +58,6 @@ async function doStartVpn(ipv4Addr: string, cidr: number, routes: string[], dns?
     ipv4Addr: `${ipv4Addr}/${cidr}`,
     routes,
     dns,
-    disallowedApplications: ['com.kkrainbow.easytier'],
     mtu: 1300,
   })
   if (start_ret?.errorMsg?.length) {
